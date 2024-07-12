3d4b8618b90293695df33b7dba2d97c9fde71a3a
davidsrichards <160143242+davidsrichards@users.noreply.github.com>
2024-07-13 00:42:09 +0100

    Update google-user.mjs

--- server/google-OAUTH/google-user/google-user.mjs
+++ server/google-OAUTH/google-user/google-user.mjs
@@ -6,9 +6,9 @@ const googleRout = express.Router();
 
 ///// URLS
 
-const failureMessage = "http://localhost:5173/login/error";
-const successRedirect = "http://localhost:5173/google/login/success";
-const failureRedirect = "http://localhost:5173/failure";
+const failureMessage = "https://my-quiz-app-1.onrender.com/login/error";
+const successRedirect = "https://my-quiz-app-1.onrender.com/google/login/success";
+const failureRedirect = "https://my-quiz-app-1.onrender.com/failure";
 
 /// verification
 
