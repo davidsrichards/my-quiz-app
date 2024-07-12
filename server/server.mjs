02b1fd0f40a187241ee3e477b0f98a64fe884266
davidsrichards <160143242+davidsrichards@users.noreply.github.com>
2024-07-12 21:17:09 +0100

    Update server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -18,7 +18,7 @@ import cors from "cors";
 const PORT = 5000;
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: " http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://my-quiz-app-1.onrender.com", credentials: true }));
 app.use(cookieParser("daverich"));
 
 ////////////////
