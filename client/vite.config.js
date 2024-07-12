541a9d2f5c64007aa17def640cf733d644b6e9a5
davidsrichards <160143242+davidsrichards@users.noreply.github.com>
2024-07-12 21:00:07 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api/user": "http://localhost:5000/",
+      "/api/user": "https://cbt-app-pecj.onrender.com",
     },
   },
   plugins: [react()],
